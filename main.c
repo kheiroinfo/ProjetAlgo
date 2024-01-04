86bef668e5c91c595afe603e3ddf89d11930a804
Samy <samyhabaz@gmail.com>
2024-01-04 21:07:41 +0100

    fonction insérer noeud

--- main.c
+++ main.c
@@ -18,3 +18,14 @@ Node* newNode(int data) {
     node->color = WHITE; // Default color
     return node;
 }
+
+Node* insert(Node* node, int data) {
+    if (node == NULL) return newNode(data);
+
+    if (data < node->data)
+        node->left = insert(node->left, data);
+    else if (data > node->data)
+        node->right = insert(node->right, data);
+
+    return node;
+}
