d5d18b7e24f1865dbca0646eeb4e0ddab6d0d098
kheiroinfo <133672577+kheiroinfo@users.noreply.github.com>
2024-01-05 22:00:15 +0100

    fonction drawtree and drawnode

--- main.c
+++ main.c
@@ -47,3 +47,23 @@ void drawNode(int x, int y, int data, Color color) {
     DrawRectangle(x, y, 50, 50, DARKGRAY);  // Set node color to DARKGRAY
     DrawText(TextFormat("%d", data), x + 15, y + 20, 20, color);
 }
+void drawArrow(Vector2 start, Vector2 end, Color color) {
+    DrawLineV(start, end, color);
+    DrawTriangle(end, (Vector2){end.x - 10, end.y - 5}, (Vector2){end.x - 10, end.y + 5}, color);
+}
+
+void drawTree(Node* root, int x, int y, int horizontalSpacing) {
+    if (root == NULL) return;
+
+    drawNode(x, y, root->data, root->color);
+
+    if (root->left) {
+        drawArrow((Vector2){x + 25, y + 50}, (Vector2){x - horizontalSpacing + 25, y + 100}, DARKGRAY);
+        drawTree(root->left, x - horizontalSpacing, y + 100, horizontalSpacing / 2);
+    }
+
+    if (root->right) {
+        drawArrow((Vector2){x + 25, y + 50}, (Vector2){x + horizontalSpacing + 25, y + 100}, DARKGRAY);
+        drawTree(root->right, x + horizontalSpacing, y + 100, horizontalSpacing / 2);
+    }
+}
